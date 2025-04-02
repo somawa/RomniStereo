09c3512df7a06ab9494a34edd465aca7eaa114e6
amos <aa.gyang88@gmail.com>
2025-04-02 16:18:28 +0800

    updated download.sh without rm commands

--- download.sh
+++ download.sh
@@ -1,23 +1,23 @@
 # downloading and preparing the five datasets
 
 cd ../omnidata
-unzip urban_sunny.zip && rm urban_sunny.zip
-unzip urban_cloudy.zip && rm urban_cloudy.zip
-unzip urban_sunset.zip && rm urban_sunset.zip
-unzip urban_omnidepth_gt_640.zip && rm urban_omnidepth_gt_640.zip
+unzip urban_sunny.zip # && rm urban_sunny.zip
+unzip urban_cloudy.zip # && rm urban_cloudy.zip
+unzip urban_sunset.zip # && rm urban_sunset.zip
+unzip urban_omnidepth_gt_640.zip # && rm urban_omnidepth_gt_640.zip
 cp -r omnidepth_gt_640 ./sunny/ && cp -r omnidepth_gt_640 ./cloudy/ && mv omnidepth_gt_640 ./sunset/
 cp urban_config.yaml ./sunny/config.yaml && cp urban_config.yaml ./cloudy/config.yaml && cp urban_config.yaml ./sunset/config.yaml 
 rm urban_config.yaml
 
 
-unzip omnihouse.zip && rm omnihouse.zip
-unzip house_omnidepth_gt_640.zip && rm house_omnidepth_gt_640.zip
+unzip omnihouse.zip # && rm omnihouse.zip
+unzip house_omnidepth_gt_640.zip # && rm house_omnidepth_gt_640.zip
 mv omnidepth_gt_640 ./omnihouse/
 mv house_config.yaml ./omnihouse/config.yaml
 
 
-unzip omnithings.zip && rm omnithings.zip
-unzip things_omnidepth_gt_640.zip && rm things_omnidepth_gt_640.zip
+unzip omnithings.zip # && rm omnithings.zip
+unzip things_omnidepth_gt_640.zip # && rm things_omnidepth_gt_640.zip
 mv omnidepth_gt_640 ./omnithings/
 wget -c http://cvlab.hanyang.ac.kr/project/omnistereo/data/things_config.yaml
 mv things_config.yaml ./omnithings/config.yaml
